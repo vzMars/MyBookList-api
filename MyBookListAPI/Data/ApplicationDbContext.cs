4f69e915131391eed5f443d96d36fc5447dcee78
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-15 21:53:02 -0400

    dbcontext added

---
+++ MyBookListAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyBookListAPI.Models;
+
+namespace MyBookListAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
