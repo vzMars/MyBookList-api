8611ac4b042f2d4222c49e65acf4b9a33fd51da1
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-15 20:32:31 -0400

    Book model added which has a one to many relationship with the ApplicationUser model (one user can have many books)

---
+++ MyBookListAPI/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace MyBookListAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public string Cover { get; set; }
+        public Status Status { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
