a0b0e6ce5fc65e1850c2f771e90eb99774bf14b4
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-15 20:24:42 -0400

    ApplicationUser model that extends IdentityUser and contains a Books collection property

---
+++ MyBookListAPI/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyBookListAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<Book> Books { get; set; }
+    }
+}
