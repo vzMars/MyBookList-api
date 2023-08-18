2d4272dbd7f44d2c207b51648f976996d9684025
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-18 09:07:13 -0400

    dto request body objects for the login and signup actions in the authcontroller

---
+++ MyBookListAPI/Dto/SignupRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBookListAPI.Dto
+{
+    public class SignupRequest
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
