3ad68acffbcd3d9b43bbd09f3fcf409603fd940c
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-17 22:19:16 -0400

    application dbcontext updated to add many to many relationships

--- MyBookListAPI/Data/ApplicationDbContext.cs
+++ MyBookListAPI/Data/ApplicationDbContext.cs
@@ -11,5 +11,21 @@ namespace MyBookListAPI.Data
         }
 
         public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<BookUser> BookUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Book>()
+                .HasMany(e => e.Authors)
+                .WithMany(e => e.Books)
+                .UsingEntity<BookAuthor>();
+            modelBuilder.Entity<Book>()
+                .HasMany(e => e.Users)
+                .WithMany(e => e.Books)
+                .UsingEntity<BookUser>();
+        }
     }
 }
