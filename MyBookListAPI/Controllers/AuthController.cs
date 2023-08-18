f667bad8fcf6cbd1aaa963928d9cd0a592565d06
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-18 08:57:41 -0400

    authcontroller with actions that return a string containing the name of the method

---
+++ MyBookListAPI/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBookListAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> AuthStatus()
+        {
+            return Ok("auth status");
+        }
+
+        [HttpPost("Login")]
+        public ActionResult<string> Login()
+        {
+            return Ok("login");
+        }
+
+        [HttpGet("Logout")]
+        public ActionResult<string> Logout()
+        {
+            return Ok("logout");
+        }
+
+        [HttpPost("Signup")]
+        public ActionResult<string> Signup()
+        {
+            return Ok("signup");
+        }
+    }
+}
