1d39a94306820b38980c2b86e9aae119ceff4ef3
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-18 13:30:00 -0400

    updated the cookie authentication redirects to instead return to the client a response of 401(login) or 403(acess denied)

--- MyBookListAPI/Program.cs
+++ MyBookListAPI/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MyBookListAPI.Data;
+using MyBookListAPI.Interfaces;
 using MyBookListAPI.Models;
+using MyBookListAPI.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,8 +31,25 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Events.OnRedirectToAccessDenied = context =>
+    {
+        context.Response.StatusCode = 403;
+        return Task.CompletedTask;
+    };
+
+    options.Events.OnRedirectToLogin = context =>
+    {
+        context.Response.StatusCode = 401;
+        return Task.CompletedTask;
+    };
+});
 
 var app = builder.Build();
 
