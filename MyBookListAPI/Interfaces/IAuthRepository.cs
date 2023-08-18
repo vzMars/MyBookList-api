cb2b0fa77be905bdd3f45a6acb2bfbbdfba74403
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-18 13:19:28 -0400

    AuthRepository added with login, signup and logout methods

---
+++ MyBookListAPI/Interfaces/IAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyBookListAPI.Dto;
+
+namespace MyBookListAPI.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<AuthResponse> Login(LoginRequest request);
+        Task<AuthResponse> Logout();
+        Task<AuthResponse> Signup(SignupRequest request);
+    }
+}
---
+++ MyBookListAPI/Repository/AuthRepository.cs
@@ -0,0 +1,23 @@
+﻿using MyBookListAPI.Dto;
+using MyBookListAPI.Interfaces;
+
+namespace MyBookListAPI.Repository
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public Task<AuthResponse> Login(LoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthResponse> Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthResponse> Signup(SignupRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
