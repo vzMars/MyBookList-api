2caa518a65624c0d2c3e97fd9f6fd261b81bbe0b
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-18 09:00:03 -0400

    response object for the auth controller which if sucessful returns a user otherwise null with a error message

---
+++ MyBookListAPI/Dto/AuthResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBookListAPI.Dto
+{
+    public class AuthResponse
+    {
+        public User? User { get; set; }
+        public bool Success { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+    }
+}
