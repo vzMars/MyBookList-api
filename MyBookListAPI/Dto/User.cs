697729043178b559ce69a5e49deed032690a9147
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-18 08:58:30 -0400

    user dto that doesnt expose the password of the user

---
+++ MyBookListAPI/Dto/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBookListAPI.Dto
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
