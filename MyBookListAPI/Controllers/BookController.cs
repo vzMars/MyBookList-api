0a02a2401449fe8fa933122551ccf633a3186640
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-18 20:41:07 -0400

    Added the BookController with a bunch of routes that for now return the name of the method the request was made to

---
+++ MyBookListAPI/Controllers/BookController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBookListAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> GetBooks()
+        {
+            return Ok("get books route");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> GetBook(int id)
+        {
+            return Ok("get book route");
+        }
+
+        [HttpGet("search/{query}")]
+        public async Task<ActionResult<string>> SearchBooks(string query)
+        {
+            return Ok("search books route");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> AddBook()
+        {
+            return Ok("add book route");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<string>> UpdateBookStatus(int id)
+        {
+            return Ok("update book status route");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<string>> DeleteBook(int id)
+        {
+            return Ok("delete book route");
+        }
+    }
+}
