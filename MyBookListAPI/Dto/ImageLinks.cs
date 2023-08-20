06c8005615c91860cdcf16904322bc6ac5c317c7
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-20 09:13:08 -0400

    VolumeInfo dto now contains image links

---
+++ MyBookListAPI/Dto/ImageLinks.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MyBookListAPI.Dto
+{
+    public class ImageLinks
+    {
+        [JsonPropertyName("thumbnail")]
+        public string Thumbnail { get; set; } = string.Empty;
+
+        [JsonPropertyName("smallThumbnail")]
+        public string SmallThumbnail { get; set; } = string.Empty;
+    }
+}
--- MyBookListAPI/Dto/VolumeInfo.cs
+++ MyBookListAPI/Dto/VolumeInfo.cs
@@ -8,9 +8,12 @@ namespace MyBookListAPI.Dto
         public string Title { get; set; }
 
         [JsonPropertyName("authors")]
-        public ICollection<string>? Authors { get; set; }
+        public ICollection<string> Authors { get; set; } = new List<string>();
 
         [JsonPropertyName("description")]
-        public string? Description { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        [JsonPropertyName("imageLinks")]
+        public ImageLinks? ImageLinks { get; set; }
     }
 }
