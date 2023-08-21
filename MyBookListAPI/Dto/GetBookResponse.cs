6e3b433497f4295039b4643ec6049f85551ae02f
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-20 20:30:44 -0400

    response dto for when user sends a request to a specific book

---
+++ MyBookListAPI/Dto/GetBookResponse.cs
@@ -0,0 +1,13 @@
+﻿using MyBookListAPI.Models;
+
+namespace MyBookListAPI.Dto
+{
+    public class GetBookResponse
+    {
+        public VolumeInfo? Details { get; set; }
+        public Status? Status { get; set; }
+        public bool Success { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+
+    }
+}
