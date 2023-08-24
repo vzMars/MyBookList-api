012aadfba695bf824b9b0fbcfbd72451ca087194
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-08-23 21:41:33 -0400

    dto that is used to return the user profile if found which contains their books and username

---
+++ MyBookListAPI/Dto/UserProfileResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBookListAPI.Dto
+{
+    public class UserProfileResponse
+    {
+        public ICollection<BookUserItem> Books { get; set; } = new List<BookUserItem>();
+        public string Username { get; set; }
+        public bool Success { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+    }
+}
